7e544139e51a84a8c0e3c11d96bac785768f61b6
majj <aidear@163.com>
2014-08-12 12:01:53 +0800

    heka logstream

--- py/heka/heka_tcp.py
+++ py/heka/heka_tcp.py
@@ -1,40 +1,88 @@
 # -*- coding: utf-8 -*-
 
 """
-tcp client(NetworkInput) for heka
+heka tcp client(NetworkInput)
 
+python - hekad
+
+- can't connect to hekad/hekad not start:
 [Errno 10061] No connection could be made because
     the target machine actively refused it
 
+save data in redis/logfile when no connection?
+
 """
+import logbook
 
 import socket
 import gevent
 
+import toml
+
+logbook.set_datetime_format("local")
+
+logger = logbook.Logger('hekac')
+
+#log = logbook.FileHandler('heka_tcp.log')
+
+log = logbook.RotatingFileHandler('heka_tcp.log', max_size=1024, backup_count=5)
+
+log.push_application()
+
+
+def get_conf(conf_fn):
+    
+    """ get configuration from .toml"""
+    
+    
+    with open(conf_fn) as conf_fh:
+        
+        conf = toml.loads(conf_fh.read())
+        
+        #print(config)
+        return conf
+        
+        
+        
 class SocketError(Exception):
     """ heka not started or wrong port"""
     pass
+    
 
-class TCPClient(object):
+
+class HekaTCPClient(object):
     """ tcp input """
     
     def __init__(self):
         """ init """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
+        self.conf = get_conf("hekac.toml")
+        
+        self.host = self.conf["client"]["host"]
+        self.port = self.conf["client"]["port"]
+        
     def connect(self):
         """ connect """
         
         try:
-            self.sock.connect(('127.0.0.1', 5565))
+            
+
+            
+            self.sock.connect((self.host, self.port))
             
             print "connected"
             
         except Exception as err:
 
             print dir(err)
+            
             print err.errno
-            errmsg =  err.strerror
+            
+            errmsg =  "target: %s:%s, %s" % (self.host, self.port, err.strerror)
+            
+            logger.error(errmsg*3)
+            
             raise SocketError(errmsg)
 
     def send(self, i):      
@@ -60,14 +108,19 @@ def main():
     """ main """
     #time.sleep(1)
     
-    client = TCPClient()
+    logger.debug("heka tcp")
+    
+    client = HekaTCPClient()
     
     client.connect()
 
-    for i in xrange(1, 33):
+    for i in xrange(1, 3333):
         
         client.send(i)   
-        print "send"
+        logger.debug("send")
+        logger.debug("=="*20)
+        logger.info(i)
+        
         gevent.sleep(1)
     
     
